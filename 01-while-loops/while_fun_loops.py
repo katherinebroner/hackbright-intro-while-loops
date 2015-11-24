84f8599cf50fde39326634bff50989e2c46e4530
pk <paceko@outlook.com>
2015-11-23 19:58:02 -0800

    questions 1 thru 5

---
+++ 01-while-loops/while_fun_loops.py
@@ -0,0 +1,36 @@
+# count = 1
+# while count < 21:
+#     print count
+#     count += 1
+
+# count = 1
+# while count < 21:
+#     if count == 13:
+#         print "hello"
+#     else:
+#         print count
+#     count += 1
+
+# count = 0
+# while count < 101:
+#     print count 
+#     count += 10
+
+# count = 0
+# while count <= 10:
+#     if count % 2 != 0:
+#         print count
+#     count += 1
+
+count = 10
+while count >= 0 and count <=10:
+    if count == 0:
+        print "Blastoff!"
+    else:     
+        print count
+    count -= 1
+
+    
+
+
+
